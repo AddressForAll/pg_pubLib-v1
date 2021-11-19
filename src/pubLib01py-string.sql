1211b5a13c057589a39f4fa041305120138a3798
Peter <ppkrauss@gmail.com>
2021-11-19 08:46:14 -0300

    ops

--- src/pubLib01py-string.sql
+++ src/pubLib01py-string.sql
@@ -5,13 +5,9 @@
 
 CREATE EXTENSION plpythonu;
 
+-- str_to_normalized_unicode
 CREATE or replace FUNCTION unicode_normalize(str text) RETURNS text as $f$
   # check details at https://stackoverflow.com/questions/24863716
   import unicodedata
   return unicodedata.normalize('NFC', str.decode('UTF-8'))
 $f$ LANGUAGE PLPYTHONU STRICT;
-
-CREATE or replace FUNCTION to_integer(str text) RETURNS int as $f$
-  SELECT CASE WHEN s='' THEN NULL::int ELSE s::int END
-  FROM (SELECT regexp_replace(str, '[^0-9]', '','g')) t(s) 
-$f$ LANGUAGE SQL IMMUTABLE;
