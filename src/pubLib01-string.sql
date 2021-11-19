f476ca305ac4fc87ae698f589cbf3d1ee1f93f52
Peter <ppkrauss@gmail.com>
2021-11-19 08:47:56 -0300

    add to_integer()

--- src/pubLib01-string.sql
+++ src/pubLib01-string.sql
@@ -1,6 +1,16 @@
 
 -- -- -- -- -- -- -- --
+-- Casts, str_to_something:
+
+CREATE or replace FUNCTION to_integer(str text) RETURNS int as $f$
+  SELECT CASE WHEN s='' THEN NULL::int ELSE s::int END
+  FROM (SELECT regexp_replace(str, '[^0-9]', '','g')) t(s) 
+$f$ LANGUAGE SQL IMMUTABLE;
+
+-- -- -- -- -- -- -- --
 -- Array-aggregators:
+
+-- string lib??
 
 CREATE or replace FUNCTION to_hex( p_x bigint[], p_fill_zeros int DEFAULT NULL) RETURNS text[] AS $f$
   SELECT array_agg( CASE WHEN $2>0 THEN lpad(x,p_fill_zeros,'0') ELSE x END )
