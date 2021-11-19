7f8c2db0444faff125c9bd3bfc4ebd7487c440c5
Peter <ppkrauss@gmail.com>
2021-11-19 08:44:59 -0300

    add to_integer()

--- src/pubLib01py-string.sql
+++ src/pubLib01py-string.sql
@@ -10,3 +10,8 @@ CREATE or replace FUNCTION unicode_normalize(str text) RETURNS text as $f$
   import unicodedata
   return unicodedata.normalize('NFC', str.decode('UTF-8'))
 $f$ LANGUAGE PLPYTHONU STRICT;
+
+CREATE or replace FUNCTION to_integer(str text) RETURNS int as $f$
+  SELECT CASE WHEN s='' THEN NULL::int ELSE s::int END
+  FROM (SELECT regexp_replace(str, '[^0-9]', '','g')) t(s) 
+$f$ LANGUAGE SQL IMMUTABLE;
