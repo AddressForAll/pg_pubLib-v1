457157f993fedae88381b6ecbd4e186bb94719f6
Peter <ppkrauss@gmail.com>
2021-11-25 17:08:39 -0300

    add yaml_to_jsonb

--- src/pubLib01py-string.sql
+++ src/pubLib01py-string.sql
@@ -11,3 +11,10 @@ CREATE or replace FUNCTION unicode_normalize(str text) RETURNS text as $f$
   import unicodedata
   return unicodedata.normalize('NFC', str.decode('UTF-8'))
 $f$ LANGUAGE PLPYTHONU STRICT;
+
+CREATE or replace FUNCTION yaml_to_jsonb(p_yaml text) RETURNS jsonb AS $f$
+    import yaml
+    import json
+
+    return json.dumps( yaml.safe_load(p_yaml) )
+$f$ LANGUAGE plpython3u IMMUTABLE;
