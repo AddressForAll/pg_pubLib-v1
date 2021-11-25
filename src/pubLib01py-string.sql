4fc0331698b7c7f35d9aaad89b55ffed384bc39c
Peter <ppkrauss@gmail.com>
2021-11-25 17:39:41 -0300

    add jsonb_mustache_render

--- src/pubLib01py-string.sql
+++ src/pubLib01py-string.sql
@@ -3,19 +3,49 @@
 -- -- -- -- -- --
 -- Python3 Wraps
 
--- CREATE EXTENSION plpythonu;
+-- old CREATE EXTENSION plpythonu;
 CREATE extension IF NOT EXISTS PLpython3u;  -- Python3 untrusted.
+-- SELECT * FROM pg_language;
 
--- str_to_normalized_unicode
-CREATE or replace FUNCTION unicode_normalize(str text) RETURNS text as $f$
+CREATE or replace FUNCTION py3_return_version() RETURNS text AS $$
+    import sys
+    return sys.version
+$$ LANGUAGE plpython3u;
+-- SELECT * FROM ingest.py3_return_version();
+
+-- old unicode_normalize:
+CREATE or replace FUNCTION str_to_normalized_unicode(str text) RETURNS text as $f$
   # check details at https://stackoverflow.com/questions/24863716
   import unicodedata
   return unicodedata.normalize('NFC', str.decode('UTF-8'))
-$f$ LANGUAGE PLpython3u STRICT;
+$f$ LANGUAGE PLpython3u STRICT IMMUTABLE;  -- need STRICT?
 
 CREATE or replace FUNCTION yaml_to_jsonb(p_yaml text) RETURNS jsonb AS $f$
     import yaml
     import json
-
     return json.dumps( yaml.safe_load(p_yaml) )
 $f$ LANGUAGE PLpython3u IMMUTABLE;
+
+-- jsonb_to_yaml?
+
+-------
+
+CREATE or replace FUNCTION jsonb_mustache_render(
+  tpl text,  -- input Mustache template
+  i jsonb,   -- input content
+  partials_path text DEFAULT ''  --?? must be a standard like /tmp/pg_io/mustache_partials
+) RETURNS text AS $f$
+  import chevron
+  import json
+  j = json.loads(i)
+  return chevron.render(tpl,j,partials_path)
+$f$ LANGUAGE plpython3u IMMUTABLE;
+-- SELECT ingest.mustache_render('Hello, {{ mustache }}!', '{"mustache":"World"}'::jsonb);
+
+----
+
+CREATE or replace FUNCTION yamlfile_to_jsonb(file text) RETURNS JSONb AS $wrap$
+  SELECT yaml_to_jsonb( pg_read_file(file) )
+$wrap$ LANGUAGE SQL IMMUTABLE;
+
+
