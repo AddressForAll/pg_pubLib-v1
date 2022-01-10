8abcd6ad53d9e4420fe02d332b2aa8078bef4eb4
Peter <ppkrauss@gmail.com>
2022-01-10 12:48:50 -0300

    add geohash_GeomMosaic

--- src/pubLib05pgis-geohash.sql
+++ src/pubLib05pgis-geohash.sql
@@ -15,3 +15,22 @@ CREATE or replace FUNCTION geohash_GeomsFromPrefix(
   SELECT prefix||x, ST_SetSRID( ST_GeomFromGeoHash(prefix||x), 4326)
   FROM unnest('{0,1,2,3,4,5,6,7,8,9,b,c,d,e,f,g,h,j,k,m,n,p,q,r,s,t,u,v,w,x,y,z}'::text[]) t(x)
 $f$ LANGUAGE SQL IMMUTABLE;
+
+
+CREATE or replace FUNCTION geohash_GeomMosaic(ghs_array text[])
+RETURNS TABLE(ghs text, lghs int, geom geometry) AS $f$
+  WITH ghsgeom AS (
+    SELECT ghs, length(ghs) as lghs, ST_GeomFromGeoHash(ghs) AS geom
+    FROM unnest(ghs_array) t1(ghs)
+  ),
+  lghsgeom AS (
+      SELECT lghs, ST_UNION(geom) as ugeom
+      FROM ghsgeom GROUP BY 1 ORDER BY 1
+  )
+    SELECT g.ghs, g.lghs, COALESCE(
+         (SELECT ST_Difference(g.geom,ST_UNION(ugeom)) FROM lghsgeom WHERE lghs>g.lghs),
+         g.geom
+       ) AS geom
+    FROM lghsgeom l INNER JOIN ghsgeom g ON l.lghs=g.lghs
+$f$ LANGUAGE SQL IMMUTABLE;
+-- SELECT geohash_GeomMosaic(array['7h2','7h2w','7h2wju','7h2wjv','7h2wjx','7h2wjy','7h2wjz','7h2wjz', '6urz']);
