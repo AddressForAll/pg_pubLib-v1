9387490ab228944ff22f5a9108297204eeabc336
cchness <cness@riseup.net>
2022-02-21 17:35:34 -0300

    Ajuste no mosaico.

--- src/pubLib05pgis-geohash.sql
+++ src/pubLib05pgis-geohash.sql
@@ -62,9 +62,9 @@ CREATE or replace FUNCTION geohash_GeomsMosaic_jinfo(ghs_set jsonB, geom_mask ge
 RETURNS TABLE(ghs text, info jsonb, geom geometry) AS $wrap$
   SELECT ghs,
          CASE jsonb_typeof(ghs_set->ghs)
-            WHEN 'null' THEN jsonb_build_object('lghs',lghs)
-            WHEN 'object' THEN (ghs_set->ghs) || jsonb_build_object('lghs',lghs)
-            ELSE jsonb_build_object('lghs',lghs, 'val',ghs_set->ghs)
+            WHEN 'null' THEN jsonb_build_object('ghs_len',lghs)
+            WHEN 'object' THEN (ghs_set->ghs) || jsonb_build_object('ghs_len',lghs)
+            ELSE jsonb_build_object('ghs_len',lghs, 'ghs_items',ghs_set->ghs)
          END,
          geom
   FROM geohash_GeomsMosaic(  (SELECT array_agg(k) FROM jsonb_object_keys(ghs_set) t(k)),  geom_mask   )
@@ -86,10 +86,10 @@ RETURNS TABLE(ghs text, info jsonb, geom geometry) AS $wrap$
             (SELECT jsonb_object_agg(
                  CASE WHEN substr(opt,1,7)='density' THEN (opts->>opt)||'_'||opt ELSE opt END,
                  CASE opt
-                     WHEN 'area'        THEN  round(l.area,0.01)
-                     WHEN 'area_km2'    THEN  round(l.area/1000000.0,0.01)
-                     WHEN 'density'     THEN  round((info->(opts->>opt))::float / l.area,0.01)
-                     WHEN 'density_km2' THEN  round(1000000.0*(info->(opts->>opt))::float / l.area,0.01)
+                     WHEN 'area'        THEN  l.area
+                     WHEN 'area_km2'    THEN  l.area/1000000.0
+                     WHEN 'density'     THEN  (info->(opts->>opt))::float / l.area
+                     WHEN 'density_km2' THEN  1000000.0*(info->(opts->>opt))::float / l.area
                      ELSE null
                  END) -- \agg
              FROM jsonb_object_keys(opts) t(opt) WHERE opts is not null AND opts!='{}'::jsonb
